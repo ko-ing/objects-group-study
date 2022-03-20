e683916553085e2793c7babdf316ebe4e8214637
ko-ing <318921@naver.com>
2022-03-20 20:31:55 +0900

    잘못된 타입 변경

--- src/com/koing/two/Movie.java
+++ src/com/koing/two/Movie.java
@@ -1,6 +1,6 @@
 package com.koing.two;
 
-import com.koing.two.discountPolicy.DefaultDiscountPolicy;
+import com.koing.two.discountPolicy.DiscountPolicy;
 
 import java.time.Duration;
 
@@ -8,9 +8,9 @@ public class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
-    private DefaultDiscountPolicy discountPolicy;
+    private DiscountPolicy discountPolicy;
 
-    public Movie(String title, Duration runningTime, Money fee, DefaultDiscountPolicy discountPolicy) {
+    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
         this.title = title;
         this.runningTime = runningTime;
         this.fee = fee;
